5a060f4d13a1fc972e6a9c977f1319586747b9b4
Ivan Klochkov <Klochkov.Ivan.Sergeevich@gmail.com>
2023-05-10 20:51:19 +0300

    fix: update nav-links and nav-close ico

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 (function () {
     const header = document.querySelector('.header');
     window.onscroll = () => {
-        if(window.scrollY > 50) {
+        if (window.pageYOffset > 50) {
             header.classList.add('header__active');
         } else {
             header.classList.remove('header__active');
